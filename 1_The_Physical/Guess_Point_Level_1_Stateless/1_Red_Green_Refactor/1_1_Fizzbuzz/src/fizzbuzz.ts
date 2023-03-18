7c90bb86299842ffc25879ec80f1e11244713708
Warren Eaton <warren.eaton.sciaccaluga@gmail.com>
2023-03-18 12:49:58 -0300

    [g]: handles numbers greater than 100

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,4 @@
 export function fizzBuzz(numbers: number[]) {
   for (const number of numbers)
-    if (number < 1) throw new Error("Number out of range");
+    if (number < 1 || number > 100) throw new Error("Number out of range");
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,4 +5,8 @@ describe("fizzbuzz", () => {
   it("should be able to tell that the number '0' is not in range", () => {
     expect(() => fizzBuzz([0])).toThrow(Error("Number out of range"));
   });
+
+  it("should be able to tell that the number '101' is not in range", () => {
+    expect(() => fizzBuzz([101])).toThrow(Error("Number out of range"));
+  });
 });
